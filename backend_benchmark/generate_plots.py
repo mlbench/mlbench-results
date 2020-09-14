0f785766ca5dfce2c7f9fc85bf63b1beac7384f0
Edoardo Holzl <edoardo.holzl@epfl.ch>
2020-09-14 16:53:44 +0200

    Add graphs and code to generate them

--- backend_benchmark/generate_plots.py
+++ backend_benchmark/generate_plots.py
@@ -9,10 +9,10 @@ import click
 
 # Files needed to generate the plots
 RESULT_FILES = {
-    'result_val_tensor_size @ {}.json': "tensor_size",
-    'result_val_cuda @ {}.json': "cuda",
-    'result_val_dtype @ {}.json': "dtype",
-    'result_val_avg_time @ {}.json': "avg_time"
+    "result_val_tensor_size @ {}.json": "tensor_size",
+    "result_val_cuda @ {}.json": "cuda",
+    "result_val_dtype @ {}.json": "dtype",
+    "result_val_avg_time @ {}.json": "avg_time",
 }
 
 
@@ -43,7 +43,7 @@ def read_values_from_json(json_list):
     Returns:
         list[float]: The list of values, converted to float
     """
-    return [float(x['value']) for x in json_list]
+    return [float(x["value"]) for x in json_list]
 
 
 def extract_values_for_worker(zip_file, worker):
@@ -59,7 +59,9 @@ def extract_values_for_worker(zip_file, worker):
     result = {}
     values_len = -1
     for k, v in RESULT_FILES.items():
-        values = read_values_from_json(read_json_file_from_zip(zip_file, k.format(worker)))
+        values = read_values_from_json(
+            read_json_file_from_zip(zip_file, k.format(worker))
+        )
 
         # Make sure all lengths are the same
         if values_len == -1:
@@ -73,7 +75,11 @@ def extract_values_for_worker(zip_file, worker):
 
     result = pd.DataFrame(result)
 
-    result = result.groupby(["tensor_size", "cuda", "dtype", "worker"])["avg_time"].mean().reset_index()
+    result = (
+        result.groupby(["tensor_size", "cuda", "dtype", "worker"])["avg_time"]
+        .mean()
+        .reset_index()
+    )
     return result
 
 
@@ -87,15 +93,15 @@ def parse_result_zip(filename):
         pd.DataFrame: Containing all the measurements, for all workers
     """
     zip_file = ZipFile(filename)
-    num_workers = int(filename.split('.')[-2].split('-')[-1])
+    num_workers = int(filename.split(".")[-2].split("-")[-1])
 
     dfs = []
     for i in range(num_workers):
         dfs.append(extract_values_for_worker(zip_file, i))
 
     df = pd.concat(dfs)
-    df = df.groupby(['tensor_size', 'cuda', 'dtype'])['avg_time'].mean().reset_index()
-    df['num_workers'] = num_workers
+    df = df.groupby(["tensor_size", "cuda", "dtype"])["avg_time"].mean().reset_index()
+    df["num_workers"] = num_workers
     return df
 
 
@@ -119,8 +125,8 @@ def get_backend_results(backend, directory, wildcard="metrics_{backend}-*.zip"):
 
     df = pd.concat(dfs)
     df["backend"] = backend
-    df['cuda'] = df['cuda'].apply(bool)
-    df['dtype'] = df['dtype'].apply(lambda x: "Float32" if x == 32 else "Float16")
+    df["cuda"] = df["cuda"].apply(bool)
+    df["dtype"] = df["dtype"].apply(lambda x: "Float32" if x == 32 else "Float16")
     return df
 
 
@@ -129,18 +135,32 @@ def generate_backend_analysis_plot(backend, directory, by="tensor"):
 
     workers = sorted(df["num_workers"].unique())
     if by == "workers":
-        row_order = sorted(df['dtype'].unique())
-        col_order = sorted(df['cuda'].unique())
-        g = sns.FacetGrid(row="dtype", col="cuda", hue='num_workers',
-                          data=df, height=5, sharex=True,
-                          row_order=row_order, col_order=col_order)
+        row_order = sorted(df["dtype"].unique())
+        col_order = sorted(df["cuda"].unique())
+        g = sns.FacetGrid(
+            row="dtype",
+            col="cuda",
+            hue="num_workers",
+            data=df,
+            height=5,
+            sharex=True,
+            row_order=row_order,
+            col_order=col_order,
+        )
         g.fig.suptitle("{} by number of workers".format(backend.upper()), size=16)
 
     elif by == "tensor":
-        row_order = sorted(df['dtype'].unique())
-        g = sns.FacetGrid(row="dtype", col="num_workers", hue='cuda',
-                          data=df, height=5, sharex=True,
-                          row_order=row_order, col_order=workers)
+        row_order = sorted(df["dtype"].unique())
+        g = sns.FacetGrid(
+            row="dtype",
+            col="num_workers",
+            hue="cuda",
+            data=df,
+            height=5,
+            sharex=True,
+            row_order=row_order,
+            col_order=workers,
+        )
         g.fig.suptitle("{} by tensor type".format(backend.upper()), size=16)
 
     else:
@@ -155,11 +175,13 @@ def generate_backend_analysis_plot(backend, directory, by="tensor"):
             ax.set_xscale("log")
             ax.set_yscale("log")
 
-    g.fig.subplots_adjust(top=.9)
+    g.fig.subplots_adjust(top=0.9)
     return g
 
 
-def generate_backend_comparison_plot(backends, directory, by="tensor", num_workers=0, cuda=False):
+def generate_backend_comparison_plot(
+    backends, directory, by="tensor", num_workers=0, cuda=False
+):
     dfs = []
     for backend in backends:
         df = get_backend_results(backend, directory)
@@ -168,23 +190,45 @@ def generate_backend_comparison_plot(backends, directory, by="tensor", num_worke
     df = pd.concat(dfs)
     workers = sorted(df["num_workers"].unique())
 
+    col_order = sorted(df["cuda"].unique())
+    row_order = sorted(df["dtype"].unique())
     if by == "tensor":
-        df = df[df['num_workers'] == num_workers]
-        g = sns.FacetGrid(row="dtype", col="cuda", hue='backend', data=df, height=5, sharex=True,
-                          row_order=['Float32', 'Float16'], col_order=[False, True])
-        g.fig.suptitle("Comparison of {} for {} workers".format(", ".join([b.upper() for b in backends]),
-                                                                num_workers))
+        df = df[df["num_workers"] == num_workers]
+        g = sns.FacetGrid(
+            row="dtype",
+            col="cuda",
+            hue="backend",
+            data=df,
+            height=5,
+            sharex=True,
+            row_order=row_order,
+            col_order=col_order,
+        )
+        g.fig.suptitle(
+            "Comparison of {} for {} workers".format(
+                ", ".join([b.upper() for b in backends]), num_workers
+            )
+        )
     elif by == "workers":
-        df = df[df['num_workers'].isin(workers)]
-        df = df[df['cuda'] == cuda]
+        df = df[df["num_workers"].isin(workers)]
+        df = df[df["cuda"] == cuda]
 
-        g = sns.FacetGrid(row="dtype", col="num_workers", hue='backend', data=df,
-                          height=5, sharex=True, row_order=['Float32', 'Float16'],
-                          col_order=workers)
-        g.fig.suptitle("Comparison of {} for {} workers {}".format(
-            ", ".join([b.upper() for b in backends]),
-            workers,
-            ", CUDA tensors" if cuda else "")
+        g = sns.FacetGrid(
+            row="dtype",
+            col="num_workers",
+            hue="backend",
+            data=df,
+            height=5,
+            sharex=True,
+            row_order=row_order,
+            col_order=workers,
+        )
+        g.fig.suptitle(
+            "Comparison of {} for {} workers {}".format(
+                ", ".join([b.upper() for b in backends]),
+                workers,
+                ", CUDA tensors" if cuda else "",
+            )
         )
 
     g = g.map(sns.lineplot, "tensor_size", "avg_time")
@@ -196,20 +240,29 @@ def generate_backend_comparison_plot(backends, directory, by="tensor", num_worke
             ax.set_xscale("log")
             ax.set_yscale("log")
 
-    g.fig.subplots_adjust(top=.9)
+    g.fig.subplots_adjust(top=0.9)
     return g
 
 
 @click.command()
-@click.option("--backends", default="mpi,gloo,nccl", type=str, help="Coma separated backend list")
-@click.option("--source-dir", "-s", type=str, help="Source directory containing results archives")
+@click.option(
+    "--backends", default="mpi,gloo,nccl", type=str, help="Coma separated backend list"
+)
+@click.option(
+    "--source-dir", "-s", type=str, help="Source directory containing results archives"
+)
 @click.option("--dest-dir", "-d", type=str, help="Destination directory for graphs")
-@click.option("--num-workers", default="2,4,8", type=str, help="Number of workers used for experiments (coma separated)")
+@click.option(
+    "--num-workers",
+    default="2,4,8",
+    type=str,
+    help="Number of workers used for experiments (coma separated)",
+)
 def main(backends, source_dir, dest_dir, num_workers):
     sns.set_style("whitegrid")
     click.echo("Generating all plots for backends {}".format(backends))
 
-    num_workers = [int(x) for x in num_workers.split(',')]
+    num_workers = [int(x) for x in num_workers.split(",")]
     backends = backends.split(",")
 
     for b in backends:
@@ -217,15 +270,31 @@ def main(backends, source_dir, dest_dir, num_workers):
         g_workers = generate_backend_analysis_plot(b, source_dir, by="workers")
 
         g_tensor.savefig(os.path.join(dest_dir, "{}_analysis_by_tensor.png".format(b)))
-        g_workers.savefig(os.path.join(dest_dir, "{}_analysis_by_workers.png".format(b)))
+        g_workers.savefig(
+            os.path.join(dest_dir, "{}_analysis_by_workers.png".format(b))
+        )
 
     for n in sorted(num_workers):
-        g = generate_backend_comparison_plot(backends, source_dir, by="tensor", num_workers=n)
-        g.savefig(os.path.join(dest_dir, "backends_comparison_by_tensor_{}_workers.png".format(n)))
+        g = generate_backend_comparison_plot(
+            backends, source_dir, by="tensor", num_workers=n
+        )
+        g.savefig(
+            os.path.join(
+                dest_dir, "backends_comparison_by_tensor_{}_workers.png".format(n)
+            )
+        )
 
     for cuda in [True, False]:
-        g = generate_backend_comparison_plot(backends, source_dir, by="workers", cuda=cuda)
-        g.savefig(os.path.join(dest_dir, "backends_comparison_by_workers" + ("_CUDA" if cuda else "") + ".png"))
+        g = generate_backend_comparison_plot(
+            backends, source_dir, by="workers", cuda=cuda
+        )
+        g.savefig(
+            os.path.join(
+                dest_dir,
+                "backends_comparison_by_workers" + ("_CUDA" if cuda else "") + ".png",
+            )
+        )
+
 
 if __name__ == "__main__":
     main()
